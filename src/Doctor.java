1b84c597df8f226e3f21480dcc90b02dc28fc07e
Damian Arp <damianarp@gmail.com>
2021-06-11 16:51:34 -0300

    Creando la clase Main y clase Doctor, instanciando un objeto de la clase Doctor.

---
+++ src/Doctor.java
@@ -0,0 +1,15 @@
+// Creamos la clase Doctor y sus atributos.
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    // Comportamientos
+    /**
+     * Descripción: Imprime el nombre del Doctor
+     * @return void
+     * */
+    public void showName(){
+        System.out.println("Name: " + name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Creamos la clase Main, es la clase que sirve como punto de entrada de la app.
+public class Main {
+    public static void main(String[] args) {
+
+        // Declaramos e instanciamos un objeto de la clase Doctor.
+        // Primero, tipo de objeto (nombre de la clase a la cual pertenece el objeto).
+        // Segundo, nombre del objeto.
+        // Instanciamos usando el keyword new + nombre de la clase seguido de parentesis.
+        Doctor myDoctor = new Doctor();
+
+        //Le asignamos un nombre al doctor.
+        // Accedemos al nombre a traves del atributo.
+        myDoctor.name = "Alejandro López";
+
+        //Mostramos el nombre a través del método creado en la clase Doctor.
+        myDoctor.showName(); // Name: Alejandro López
+    }
+}
